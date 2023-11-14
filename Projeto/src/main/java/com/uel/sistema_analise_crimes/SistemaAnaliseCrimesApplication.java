921329c71c76c39dd56660a23679790e9d77122b
Arthur Martins <88567104+arthmartins@users.noreply.github.com>
2023-11-14 18:48:56 -0300

    projeto criado

---
+++ Projeto/src/main/java/com/uel/sistema_analise_crimes/SistemaAnaliseCrimesApplication.java
@@ -0,0 +1,13 @@
+package com.uel.sistema_analise_crimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAnaliseCrimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAnaliseCrimesApplication.class, args);
+	}
+
+}
---
+++ Projeto/src/test/java/com/uel/sistema_analise_crimes/SistemaAnaliseCrimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uel.sistema_analise_crimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAnaliseCrimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
